bcdd5adae10cb684b5a1ed2eade7fec263e4e515
Brandon Valley <bavvh8@mst.edu>
2017-07-16 09:07:32 -0500

    IT KINA WORKS! - hits errores every once in a while and is not optimized at all

--- twitterbot.py
+++ twitterbot.py
@@ -59,7 +59,7 @@ def search(twts):
             api.create_favorite(i.id)
             print ("JUST FAVORITED " + (i.text))
         # This part waits a minute before moving onto the next one.
-        time.sleep(60)
+        time.sleep(60)#could this be 10 sec?
 
 
 def run():
