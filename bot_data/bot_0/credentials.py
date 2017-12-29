4260ae330c444d2eab2c0ef804b79528884e591e
Brandon Valley <bavvh8@mst.edu>
2017-12-29 15:18:03 -0600

    cleaned up files

--- bot_data/bot_0/credentials.py
+++
@@ -1,5 +0,0 @@
-#enter the corresponding information from your Twitter application:
-CONSUMER_KEY = 'MEpbDNplsX724pFmaKtTjlMFW' #keep the quotes, enter your consumer key
-CONSUMER_SECRET = 'BEIyPrCYzWe8X65fArKE65cXAkLtyUd8RAYlLllHrw4ubiND9T'#keep the quotes, enter your consumer secret key
-ACCESS_KEY = '932834480406122497-kEdX6h2yZ7ocWhEgVznG22My7qbeLx5'#keep the quotes, enter your access token
-ACCESS_SECRET =  '0hjm4BHFfCiHgNwNVccNw66puL0ZN22hwGinrtdjrW2d6'#keep the quotes, enter your access token secret
